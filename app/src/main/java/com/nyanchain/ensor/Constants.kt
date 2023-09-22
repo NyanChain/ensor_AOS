afdd6e74398e771e3770d3410276b3f9f196add9
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2023-09-22 18:01:27 +0900

    Fix: Home 바텀 네비게이션 qrFragment 보이게 수정

--- app/src/main/java/com/nyanchain/ensor/Constants.kt
+++ app/src/main/java/com/nyanchain/ensor/Constants.kt
@@ -14,21 +14,6 @@ object Constants {
     const val KEY_CONTENT_TYPE = "Content-Type"
     const val VALUE_CONTENT_TYPE = "application/json"
     const val KEY_AUTHORIZATION = "Authorization"
+    const val REQUEST_CODE = 100
 
-//    const val OPENAI_API_KEY = "AIzaSyCINdfAgqD_f_bmZZWvAecuuvI3vc1zsVA"
-//
-//    const val CONNECT_TIME_OUT = 15L
-//    const val READ_TIME_OUT = 15L
-//    const val WRITE_TIME_OUT = 15L
-//
-//    const val DEFAULT_BOOLEAN_VALUE = false
-//    const val DEFAULT_STRING_VALUE = ""
-//
-//    const val HTTP_OK = 200
-//    const val RESPONSE_NULL_ERROR = "Response is null"
-//    const val SERVER_CONNECTION_ERROR = "Server Connection Error"
-//    const val INTERNET_CONNECTION_ERROR = "Internet Connection Error"
-//
-//    const val STARTING_PAGE_INDEX = 1
-//    const val PAGING_SIZE = 15
 }
